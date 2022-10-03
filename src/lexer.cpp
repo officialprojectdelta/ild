f8e8fbae938ec7cb336e2df9854578c1240c4906
ProjectDelta <112292015+officialprojectdelta@users.noreply.github.com>
2022-10-03 11:23:55 -0700

    Update lexer.cpp

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -4,15 +4,28 @@
 #include "lexer.h"
 #include "error.h" // A nice exception class with printf formatting
 
+// Generates a string, returns if whitespace (' ', newline, or '\r') is found
+std::string gen_str(const std::string& src, size_t& pos)
+{
+    size_t pos1 = pos;
+    pos = src.find_first_of(" \n\r", pos);
+    return str.substr(pos1, pos++);
+}
+
 // Generate all tokens in a text subsection
-std::vector<FunList> gen_text(const std::string& src, size_t pos)
+std::vector<FunList> gen_text(const std::string& src, size_t& pos)
 {
     // Loop through characters
     // If a new function is defined, push it back and continue
-    // If a new variable is added, do nothing
-    // F
-    // Actually, 
+    // If a new variable is defined, do nothing 
+    // For other instructions, add them as normal 
     // If a dot is reached, return the funlist
+    
+    // No guarantee of compiliation, because this was written without testing
+    while (true)
+    {
+        
+    }
 }
 
 Globals lex(const std::string& src)
