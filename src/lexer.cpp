b51925788a9b97537f64a5387c04f8ecbc39444f
ProjectDelta <112292015+officialprojectdelta@users.noreply.github.com>
2022-10-03 09:41:44 -0700

    Update lexer.cpp

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -4,10 +4,15 @@
 #include "lexer.h"
 #include "error.h" // A nice exception class with printf formatting
 
-// Generate all variables in a text subsection
+// Generate all tokens in a text subsection
 std::vector<FunList> gen_text(const std::string& src, size_t pos)
 {
-
+    // Loop through characters
+    // If a new function is defined, push it back and continue
+    // If a new variable is added, do nothing
+    // F
+    // Actually, 
+    // If a dot is reached, return the funlist
 }
 
 Globals lex(const std::string& src)
