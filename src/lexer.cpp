891d8d033166ee2dab68b1fa12b3b410ad28536f
ProjectDelta <112292015+officialprojectdelta@users.noreply.github.com>
2022-10-03 14:01:28 -0700

    Update lexer.cpp

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -9,11 +9,21 @@ std::string gen_str(const std::string& src, size_t& pos)
 {
     size_t pos1 = pos;
     pos = src.find_first_of(" \n\r", pos);
-    return str.substr(pos1, pos++);
+    return str.substr(pos1, pos++ - pos1 - 1);
 }
 
-// Generate all tokens in a text subsection
-std::vector<FunList> gen_text(const std::string& src, size_t& pos)
+// MOVE THIS TO TYPE.CPP
+// Generates a type from a string
+Type gen_type(const std::string& src, size_t& pos)
+{
+    switch (gen_str) 
+    {
+            
+    }
+}
+
+// Generate all tokens in a text subsection (generates the function headers)
+std::vector<FunList> gen_funs(Globals* current, const std::string& src, size_t& pos)
 {
     // Loop through characters
     // If a new function is defined, push it back and continue
@@ -24,15 +34,33 @@ std::vector<FunList> gen_text(const std::string& src, size_t& pos)
     // No guarantee of compiliation, because this was written without testing
     while (true)
     {
-        
+        switch (gen_str(src, pos))
+        {
+            case "global":
+            {
+               if (gen_str(src, pos) != "def") throw compiler_error("Invalid function decl);
+               
+                                                                
+            }
+            default throw compiler_error("Invalid function decl");
+        }
     }
 }
 
-Globals lex(const std::string& src)
+Globals* lex(const std::string& src)
 {
+    Globals* current = new Globals;
     size_t pos = 0;
     while (pos < src.size())
     {
         if (src[pos] != '.') throw compiler_error("Unexpected character %c", src[pos]);
+        pos++;
+        switch (gen_str(src, pos))
+        {
+            case "text":
+            {
+               gen_text(current);
+            }
+        }
     }
 }
